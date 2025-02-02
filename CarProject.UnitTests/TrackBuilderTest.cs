a8d1df7d88a218d7524d62d3ca4b413372b9d394
Dominic192939 <dominic.kappelmayer@rechberger.org>
2025-02-02 22:20:04 +0100

    Added first UnitTest to TrackBuilderTest. Test is not working, RaceTrack property is missing.

--- CarProject.UnitTests/TrackBuilderTest.cs
+++ CarProject.UnitTests/TrackBuilderTest.cs
@@ -11,5 +11,13 @@ namespace CarProject.UnitTests
     [TestClass]
     public class TrackBuilderTest
     {
+        [TestMethod]
+        public void ItShouldBuildAConnectedTrack_ValidateStartSectionLength()
+        {
+            (int, int)[] sectionDetails = { (10, 10), (20, 20), (30, 30) };
+            TrackBuilder builder = new(sectionDetails);
+
+            Assert.AreEqual(10, builder.RaceTrack!.StartSection!.Length);
+        }
     }
 }
