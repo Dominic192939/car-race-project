986480d0541ec3334940a61996aa8258d8a9c475
Dominic192939 <dominic.kappelmayer@rechberger.org>
2025-02-02 22:24:42 +0100

    Added new UnitTest to TrackBuilderTest, it should build a connected Track and validate the given Properties correctly! And also given an looped Track (isTrackLooping: true)!

--- CarProject.UnitTests/TrackBuilderTest.cs
+++ CarProject.UnitTests/TrackBuilderTest.cs
@@ -45,5 +45,38 @@ namespace CarProject.UnitTests
             Assert.AreEqual(30, thirdSection.Length);
             Assert.AreEqual(30, thirdSection.MaxSpeed);
         }
+
+        [TestMethod]
+        public void ItShouldBuildAConnectedTrack_ValidateManualTrackMatch()
+        {
+            (int, int)[] sectionDetails = { (10, 10), (20, 20), (30, 30) };
+
+            Section startSection = new(sectionDetails[0].Item1, sectionDetails[0].Item2);
+            Section middleSection = new(sectionDetails[1].Item1, sectionDetails[1].Item2);
+            Section endSection = new(sectionDetails[2].Item1, sectionDetails[2].Item2);
+
+            startSection.AddAfterMe(middleSection);
+            middleSection.AddAfterMe(endSection);
+
+            Track manuallyBuiltTrack = new(new List<Section> { startSection, middleSection, endSection });
+            TrackBuilder builder = new(sectionDetails);
+
+            Assert.AreEqual(manuallyBuiltTrack.StartSection!.Length, builder.RaceTrack!.StartSection!.Length);
+            Assert.AreEqual(manuallyBuiltTrack.StartSection.MaxSpeed, builder.RaceTrack.StartSection.MaxSpeed);
+            Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection!.Length, builder.RaceTrack.StartSection.NextSection!.Length);
+            Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection!.MaxSpeed, builder.RaceTrack.StartSection.NextSection!.MaxSpeed);
+            Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection!.NextSection!.Length, builder.RaceTrack.StartSection.NextSection!.NextSection!.Length);
+            Assert.AreEqual(manuallyBuiltTrack.StartSection.NextSection!.NextSection!.MaxSpeed, builder.RaceTrack.StartSection.NextSection!.NextSection!.MaxSpeed);
+        }
+
+        [TestMethod]
+        public void ItShouldConnectTheLastSegmentToTheFirst_GivenAnAdditionalParameterForALoopedTrack()
+        {
+            (int, int)[] sectionDetails = { (10, 10), (30, 30) };
+
+            TrackBuilder builder = new(sectionDetails, isTrackLooping: true);
+
+            Assert.IsTrue(builder.RaceTrack!.IsLoopedTrack);
+        }
     }
 }
