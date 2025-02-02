7661d61f1cd0dcdaa576f7cae8963740107a521b
Dominic192939 <dominic.kappelmayer@rechberger.org>
2025-02-02 22:23:18 +0100

    added new unittest in TrackBuilderTest which is not working right now.

--- CarProject.UnitTests/TrackBuilderTest.cs
+++ CarProject.UnitTests/TrackBuilderTest.cs
@@ -28,5 +28,22 @@ namespace CarProject.UnitTests
 
             Assert.AreEqual(10, builder.RaceTrack!.StartSection!.MaxSpeed);
         }
+
+        [TestMethod]
+        public void ItShouldBuildAConnectedTrack_ValidateSectionConnections()
+        {
+            (int, int)[] sectionDetails = { (10, 10), (20, 20), (30, 30) };
+
+            TrackBuilder builder = new(sectionDetails);
+            Section firstSection = builder.RaceTrack!.StartSection!;
+            Section secondSection = firstSection.NextSection!;
+            Section thirdSection = secondSection.NextSection!;
+
+            Assert.AreEqual(20, secondSection.Length);
+            Assert.AreEqual(20, secondSection.MaxSpeed);
+
+            Assert.AreEqual(30, thirdSection.Length);
+            Assert.AreEqual(30, thirdSection.MaxSpeed);
+        }
     }
 }
